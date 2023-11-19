105ce5a404c66c6bce26a33592ce66fa96286d0b
BroPython65 <wfreedom999@gmail.com>
2023-11-19 18:45:38 +0630

    Update funtion has been added

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_blog_display, name='all_blog_display'),
-    path('create/', views.create, name='create')
+    path('create/', views.create, name='create'),
+    path('edit/<int:pk>', views.edit, name='edit')
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from api.models import Blog
@@ -18,3 +18,18 @@ def create(request):
         # Create the blog post
         new_post = Blog.objects.create(title=title, content=content)
     return render(request, 'frontend/create.html')
+
+#Update function
+def edit(request, pk):
+    blog = Blog.objects.get(id=pk)
+    id = pk
+    if request.method == 'PUT':
+        # Handle form submission and update data
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        # Update the blog object with new data
+        blog.title = title
+        blog.content = content
+        blog.save()
+    return render(request, 'frontend/edit.html', {'blog':blog, 'id':id})
